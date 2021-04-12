e178498efdc5aa089578e9870f2ad2ddf7940649
manyomanyo <tokahashi1120@gmail.com>
2021-04-12 21:59:41 +0900

    詳細ページ設定まで

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+    before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -14,10 +16,24 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.destroy
   end
-  
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
